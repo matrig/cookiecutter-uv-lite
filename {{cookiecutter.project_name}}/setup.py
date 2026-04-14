3cb5ee057b376ae0669102b1bbfdb3880bfb3b26
Mattia Rigotti <4016834+matrig@users.noreply.github.com>
2026-04-14 11:11:50 +0200

    chore: chore: remove redundant setup.py from generated project template

--- {{cookiecutter.project_name}}/setup.py
+++
@@ -1,7 +0,0 @@
-{% set package_name = cookiecutter.project_name|lower|replace('-', '_') -%}
-from setuptools import find_packages, setup
-
-setup(
-    name="{{ package_name }}",
-    packages=find_packages(),
-)
